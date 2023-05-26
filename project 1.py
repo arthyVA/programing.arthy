11b42abf91517138a1c2ac097fe23d3612230834
arthyVA <134696740+arthyVA@users.noreply.github.com>
2023-05-26 18:36:08 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,15 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: C:/Users/DELL/Downloads/x.py ====================
+>>> age=13
+>>> print(age)
+13
+>>> 
+>>> weight=35
+>>> print(weight)
+35
+>>> 
+>>> height=145
+>>> print(height)
+145
